59503a1301400eeebb15a3e39b1bac1f76204b83
Yan Oliveira <supremoyan@gmail.com>
2024-05-06 16:21:36 -0300

    Mais ou menos

--- src/chocostock/Main.java
+++ src/chocostock/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Bem-vindo ao ChocoStock! O doce controle de vendas e estoque");
+        System.out.println("Amatongas Mais ou menos né!");
     }
 }
