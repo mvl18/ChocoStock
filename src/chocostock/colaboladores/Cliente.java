ee07aaed15842719c2c680225767fe1dd2f2c971
Marao_LT <ainaras.marao@gmail.com>
2024-05-07 23:54:49 -0300

    inicio das classes: -Colaborador -Cliente -Funcionario -Empresa -Endereco

--- src/chocostock/colaboladores/Cliente.java
+++ src/chocostock/colaboladores/Cliente.java
@@ -1,4 +1,34 @@
 package chocostock.colaboladores;
 
-public class Cliente {
+import chocostock.Endereco;
+import chocostock.Pedido;
+
+import java.util.ArrayList;
+
+public class Cliente extends Colaborador {
+    private static int id_clientes = 100000;
+    private final int id; // final, pois nao pode ser modificado apos criado
+    private ArrayList<Integer> pedidos;
+    public Cliente(String nome, String telefone, String email, Endereco endereco) {
+        super(nome, telefone, email, endereco);
+        this.id = id_clientes++;
+        this.pedidos = new ArrayList<Integer>();
+    }
+
+    public ArrayList<Integer> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(ArrayList<Integer> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    @Override
+    public String toString() { // teste de como fazer um toString() decente
+        return super.toString() +
+                "Cliente{" +
+                "id=" + id +
+                ", pedidos=" + pedidos +
+                '}';
+    }
 }
--- src/chocostock/colaboladores/Funcionario.java
+++ src/chocostock/colaboladores/Funcionario.java
@@ -1,4 +1,50 @@
 package chocostock.colaboladores;
 
-public class Funcionario {
+import chocostock.Endereco;
+
+public class Funcionario extends Colaborador {
+    private static int id_funcionarios = 100000;
+    private final int id;
+    private String cargo;
+    private float salario;
+
+    public Funcionario(String nome, String telefone, String email, Endereco endereco, String cargo) {
+        super(nome, telefone, email, endereco);
+        this.id = id_funcionarios++;
+        this.cargo = cargo;
+        defineSalario();
+    }
+
+    private void defineSalario() {
+        switch (cargo) {
+            case "gerente":
+                this.salario = 5000.0F;
+            case "chocolateiro":
+                this.salario = 4000.0F;
+            case "analista":
+                this.salario = 4500.0F;
+            case "assistente":
+                this.salario = 3000.0F;
+            case "estagiario":
+                this.salario = 1500.0F;
+            default:
+                this.salario = 0.0F;
+        }
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
 }
