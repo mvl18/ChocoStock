ec94cf3115c7b3f56d533c9d9b177b8c42841f00
Matheus Veiga <mvl18@proton.me>
2024-05-16 00:11:58 -0300

    Implementa Estado apartir do CEP, verifica email, CEP e Telefone

--- src/chocostock/auxiliar/Verifica.java
+++ src/chocostock/auxiliar/Verifica.java
@@ -1,9 +1,18 @@
 package chocostock.auxiliar;
 
 public class Verifica {
-    public static boolean Telefone(String telefone) {
-        String testando_telefone = telefone.replaceAll("\\D", "");
-        return testando_telefone.length() >= 10;
+    public static boolean isTelefone(String telefone) {
+        String teste_telefone = telefone.replaceAll("\\D", "");
+        return teste_telefone.length() >= 10;
     }
 
+    public static boolean isEmail(String email) {
+        String teste_email = "^[\\w+._]{3,}+@\\w+\\.\\w{2,}(?:\\.\\w{2,})?(?:\\.\\w{2,})?$";
+        return email.matches(teste_email);
+    }
+
+    public static boolean isCep(String cep) {
+        String teste_cep = cep.replaceAll("\\D", "");
+        return teste_cep.length() >= 7 && teste_cep.length() <= 8;
+    }
 }
