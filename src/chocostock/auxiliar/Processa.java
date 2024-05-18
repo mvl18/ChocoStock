a46ee1311d7eca0cdd8585fd6c1da596fdd4c3b9
Cl4nyz <supremoyan@gmail.com>
2024-05-17 21:50:13 -0300

    Funções "novo..." retornaram à loja! Validador input desbugado

--- src/chocostock/auxiliar/Processa.java
+++ src/chocostock/auxiliar/Processa.java
@@ -3,11 +3,15 @@ package chocostock.auxiliar;
 import java.text.Normalizer;
 
 public class Processa {
-    public static String normaliza(String entrada) {
+    public static String normalizaString(String entrada) {
         String entradaNormalizada = Normalizer.normalize(entrada, Normalizer.Form.NFD)
                 .replaceAll("\\p{InCombiningDiacriticalMarks}+", "")
                 .toLowerCase()
                 .replaceAll("\\s", "");
         return entradaNormalizada;
+    }
+
+    public static String normalizaNumero(String numero) {
+        return numero.replaceAll("[^0-9]", "");
     }
 }
