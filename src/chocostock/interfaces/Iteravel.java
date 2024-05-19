c348a99d5e4193be4b3925d340841511ffcc9257
Cl4nyz <supremoyan@gmail.com>
2024-05-19 17:14:24 -0300

    Documentação da Interface Iterável

--- src/chocostock/interfaces/Iteravel.java
+++ src/chocostock/interfaces/Iteravel.java
@@ -3,6 +3,9 @@ package chocostock.interfaces;
 import java.util.ArrayList;
 
 public interface Iteravel {
+    /**
+     * Imprime uma lista com um item em cada linha.
+     * @param lista A lista de objetos que serão impressos.*/
     default <T> String listaVertical(ArrayList<T> lista) {
         String texto = "";
         for (T objeto : lista) {
@@ -11,6 +14,9 @@ public interface Iteravel {
         return texto;
     }
 
+    /**
+     * Imprime uma lista no formato: item1, item2, item3
+     * @param lista A lista de objetos que serão impressos.*/
     default <T> String listaHorizontal(ArrayList<T> lista) {
         String out = "";
         for (int i = 0; i < lista.size(); i++)
@@ -18,6 +24,8 @@ public interface Iteravel {
         return out;
     }
 
+    /**
+     *Sobrecarga do método listHorizontal com quebra de linha ao final.*/
     default <T> String listaHorizontalQuebraLinha(ArrayList<T> lista) {
         return listaHorizontal(lista) + "\n";
     }
